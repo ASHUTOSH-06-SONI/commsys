74cc5f941482b68d3cbecef6d9576d08efb414c0
Ashutosh Soni <ashusoni1892@gmail.com>
2026-08-13 06:45:02 +0530

    Phase Key shifting, juss mental models

---
+++ src/phasekeyshifting.py
@@ -0,0 +1,307 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+#
+# SPDX-License-Identifier: GPL-3.0
+#
+# GNU Radio Python Flow Graph
+# Title: Not titled yet
+# Author: santoshsoni
+# GNU Radio version: 3.10.12.0
+
+from PyQt5 import Qt
+from gnuradio import qtgui
+from PyQt5 import QtCore
+from gnuradio import analog
+from gnuradio import blocks
+import numpy
+from gnuradio import digital
+from gnuradio import filter
+from gnuradio.filter import firdes
+from gnuradio import gr
+from gnuradio.fft import window
+import sys
+import signal
+from PyQt5 import Qt
+from argparse import ArgumentParser
+from gnuradio.eng_arg import eng_float, intx
+from gnuradio import eng_notation
+import sip
+import threading
+
+
+
+class phasekeyshifting(gr.top_block, Qt.QWidget):
+
+    def __init__(self):
+        gr.top_block.__init__(self, "Not titled yet", catch_exceptions=True)
+        Qt.QWidget.__init__(self)
+        self.setWindowTitle("Not titled yet")
+        qtgui.util.check_set_qss()
+        try:
+            self.setWindowIcon(Qt.QIcon.fromTheme('gnuradio-grc'))
+        except BaseException as exc:
+            print(f"Qt GUI: Could not set Icon: {str(exc)}", file=sys.stderr)
+        self.top_scroll_layout = Qt.QVBoxLayout()
+        self.setLayout(self.top_scroll_layout)
+        self.top_scroll = Qt.QScrollArea()
+        self.top_scroll.setFrameStyle(Qt.QFrame.NoFrame)
+        self.top_scroll_layout.addWidget(self.top_scroll)
+        self.top_scroll.setWidgetResizable(True)
+        self.top_widget = Qt.QWidget()
+        self.top_scroll.setWidget(self.top_widget)
+        self.top_layout = Qt.QVBoxLayout(self.top_widget)
+        self.top_grid_layout = Qt.QGridLayout()
+        self.top_layout.addLayout(self.top_grid_layout)
+
+        self.settings = Qt.QSettings("gnuradio/flowgraphs", "phasekeyshifting")
+
+        try:
+            geometry = self.settings.value("geometry")
+            if geometry:
+                self.restoreGeometry(geometry)
+        except BaseException as exc:
+            print(f"Qt GUI: Could not restore geometry: {str(exc)}", file=sys.stderr)
+        self.flowgraph_started = threading.Event()
+
+        ##################################################
+        # Variables
+        ##################################################
+        self.sps = sps = 4
+        self.n_taps = n_taps = 11*sps
+        self.excess_bw = excess_bw = 0.35
+        self.timing_delay = timing_delay = 0
+        self.samp_rate = samp_rate = 32000
+        self.rrc_taps_tx = rrc_taps_tx = firdes.root_raised_cosine(sps, sps, 1.0, excess_bw, n_taps)
+        self.rrc_taps_rx = rrc_taps_rx = firdes.root_raised_cosine(1.0, sps, 1.0, excess_bw, n_taps)
+        self.noise_amp = noise_amp = 0.1
+
+        ##################################################
+        # Blocks
+        ##################################################
+
+        self._timing_delay_range = qtgui.Range(0, sps-1, 1, 0, 200)
+        self._timing_delay_win = qtgui.RangeWidget(self._timing_delay_range, self.set_timing_delay, "'timing_delay'", "counter_slider", float, QtCore.Qt.Horizontal)
+        self.top_layout.addWidget(self._timing_delay_win)
+        self.qtgui_eye_sink_x_0 = qtgui.eye_sink_c(
+            1024, #size
+            samp_rate, #samp_rate
+            1, #number of inputs
+            None
+        )
+        self.qtgui_eye_sink_x_0.set_update_time(0.10)
+        self.qtgui_eye_sink_x_0.set_samp_per_symbol(4)
+        self.qtgui_eye_sink_x_0.set_y_axis(-1, 1)
+
+        self.qtgui_eye_sink_x_0.set_y_label('Amplitude', "")
+
+        self.qtgui_eye_sink_x_0.enable_tags(True)
+        self.qtgui_eye_sink_x_0.set_trigger_mode(qtgui.TRIG_MODE_FREE, qtgui.TRIG_SLOPE_POS, 0.0, 0, 0, "")
+        self.qtgui_eye_sink_x_0.enable_autoscale(False)
+        self.qtgui_eye_sink_x_0.enable_grid(False)
+        self.qtgui_eye_sink_x_0.enable_axis_labels(True)
+        self.qtgui_eye_sink_x_0.enable_control_panel(False)
+
+
+        labels = ['Signal 1', 'Signal 2', 'Signal 3', 'Signal 4', 'Signal 5',
+            'Signal 6', 'Signal 7', 'Signal 8', 'Signal 9', 'Signal 10']
+        widths = [1, 1, 1, 1, 1,
+            1, 1, 1, 1, 1]
+        colors = ['blue', 'blue', 'blue', 'blue', 'blue',
+            'blue', 'blue', 'blue', 'blue', 'blue']
+        alphas = [1.0, 1.0, 1.0, 1.0, 1.0,
+            1.0, 1.0, 1.0, 1.0, 1.0]
+        styles = [1, 1, 1, 1, 1,
+            1, 1, 1, 1, 1]
+        markers = [-1, -1, -1, -1, -1,
+            -1, -1, -1, -1, -1]
+
+
+        for i in range(2):
+            if len(labels[i]) == 0:
+                if (i % 2 == 0):
+                    self.qtgui_eye_sink_x_0.set_line_label(i, "Eye [Re{{Data {0}}}]".format(round(i/2)))
+                else:
+                    self.qtgui_eye_sink_x_0.set_line_label(i, "Eye [Im{{Data {0}}}]".format(round((i-1)/2)))
+            else:
+                self.qtgui_eye_sink_x_0.set_line_label(i, labels[i])
+            self.qtgui_eye_sink_x_0.set_line_width(i, widths[i])
+            self.qtgui_eye_sink_x_0.set_line_color(i, colors[i])
+            self.qtgui_eye_sink_x_0.set_line_style(i, styles[i])
+            self.qtgui_eye_sink_x_0.set_line_marker(i, markers[i])
+            self.qtgui_eye_sink_x_0.set_line_alpha(i, alphas[i])
+
+        self._qtgui_eye_sink_x_0_win = sip.wrapinstance(self.qtgui_eye_sink_x_0.qwidget(), Qt.QWidget)
+        self.top_layout.addWidget(self._qtgui_eye_sink_x_0_win)
+        self.qtgui_const_sink_x_0_0 = qtgui.const_sink_c(
+            1024, #size
+            "BPSK Constellation", #name
+            1, #number of inputs
+            None # parent
+        )
+        self.qtgui_const_sink_x_0_0.set_update_time(0.10)
+        self.qtgui_const_sink_x_0_0.set_y_axis((-2), 2)
+        self.qtgui_const_sink_x_0_0.set_x_axis((-2), 2)
+        self.qtgui_const_sink_x_0_0.set_trigger_mode(qtgui.TRIG_MODE_FREE, qtgui.TRIG_SLOPE_POS, 0.0, 0, "")
+        self.qtgui_const_sink_x_0_0.enable_autoscale(False)
+        self.qtgui_const_sink_x_0_0.enable_grid(False)
+        self.qtgui_const_sink_x_0_0.enable_axis_labels(True)
+
+
+        labels = ['', '', '', '', '',
+            '', '', '', '', '']
+        widths = [1, 1, 1, 1, 1,
+            1, 1, 1, 1, 1]
+        colors = ["blue", "red", "green", "black", "cyan",
+            "magenta", "yellow", "dark red", "dark green", "dark blue"]
+        styles = [0, 0, 0, 0, 0,
+            0, 0, 0, 0, 0]
+        markers = [0, 0, 0, 0, 0,
+            0, 0, 0, 0, 0]
+        alphas = [1.0, 1.0, 1.0, 1.0, 1.0,
+            1.0, 1.0, 1.0, 1.0, 1.0]
+
+        for i in range(1):
+            if len(labels[i]) == 0:
+                self.qtgui_const_sink_x_0_0.set_line_label(i, "Data {0}".format(i))
+            else:
+                self.qtgui_const_sink_x_0_0.set_line_label(i, labels[i])
+            self.qtgui_const_sink_x_0_0.set_line_width(i, widths[i])
+            self.qtgui_const_sink_x_0_0.set_line_color(i, colors[i])
+            self.qtgui_const_sink_x_0_0.set_line_style(i, styles[i])
+            self.qtgui_const_sink_x_0_0.set_line_marker(i, markers[i])
+            self.qtgui_const_sink_x_0_0.set_line_alpha(i, alphas[i])
+
+        self._qtgui_const_sink_x_0_0_win = sip.wrapinstance(self.qtgui_const_sink_x_0_0.qwidget(), Qt.QWidget)
+        self.top_layout.addWidget(self._qtgui_const_sink_x_0_0_win)
+        self._noise_amp_range = qtgui.Range(0, 1, 0.05, 0.1, 200)
+        self._noise_amp_win = qtgui.RangeWidget(self._noise_amp_range, self.set_noise_amp, "'noise_amp'", "counter_slider", float, QtCore.Qt.Horizontal)
+        self.top_layout.addWidget(self._noise_amp_win)
+        self.interp_fir_filter_xxx_0 = filter.interp_fir_filter_ccc(1, firdes.root_raised_cosine(1.0, 4, 1.0, excess_bw, n_taps))
+        self.interp_fir_filter_xxx_0.declare_sample_delay(0)
+        self.fir_filter_xxx_0 = filter.fir_filter_ccc(1, firdes.root_raised_cosine(1.0, 4, 1.0, excess_bw, n_taps))
+        self.fir_filter_xxx_0.declare_sample_delay(0)
+        self.digital_chunks_to_symbols_xx_0 = digital.chunks_to_symbols_ic([-1,1], 2)
+        self.blocks_throttle2_0 = blocks.throttle( gr.sizeof_gr_complex*1, samp_rate, True, 0 if "auto" == "auto" else max( int(float(0.1) * samp_rate) if "auto" == "time" else int(0.1), 1) )
+        self.blocks_keep_one_in_n_0_0 = blocks.keep_one_in_n(gr.sizeof_gr_complex*1, 2)
+        self.blocks_delay_0_0 = blocks.delay(gr.sizeof_gr_complex*1, 0)
+        self.blocks_add_xx_0_0 = blocks.add_vcc(1)
+        self.analog_random_source_x_0 = blocks.vector_source_i(list(map(int, numpy.random.randint(0, 2, 1000))), True)
+        self.analog_noise_source_x_0 = analog.noise_source_c(analog.GR_GAUSSIAN, 1, 0)
+
+
+        ##################################################
+        # Connections
+        ##################################################
+        self.connect((self.analog_noise_source_x_0, 0), (self.blocks_add_xx_0_0, 1))
+        self.connect((self.analog_random_source_x_0, 0), (self.digital_chunks_to_symbols_xx_0, 0))
+        self.connect((self.blocks_add_xx_0_0, 0), (self.fir_filter_xxx_0, 0))
+        self.connect((self.blocks_delay_0_0, 0), (self.blocks_keep_one_in_n_0_0, 0))
+        self.connect((self.blocks_keep_one_in_n_0_0, 0), (self.qtgui_const_sink_x_0_0, 0))
+        self.connect((self.blocks_throttle2_0, 0), (self.interp_fir_filter_xxx_0, 0))
+        self.connect((self.digital_chunks_to_symbols_xx_0, 0), (self.blocks_throttle2_0, 0))
+        self.connect((self.fir_filter_xxx_0, 0), (self.blocks_delay_0_0, 0))
+        self.connect((self.fir_filter_xxx_0, 0), (self.qtgui_eye_sink_x_0, 0))
+        self.connect((self.interp_fir_filter_xxx_0, 0), (self.blocks_add_xx_0_0, 0))
+
+
+    def closeEvent(self, event):
+        self.settings = Qt.QSettings("gnuradio/flowgraphs", "phasekeyshifting")
+        self.settings.setValue("geometry", self.saveGeometry())
+        self.stop()
+        self.wait()
+
+        event.accept()
+
+    def get_sps(self):
+        return self.sps
+
+    def set_sps(self, sps):
+        self.sps = sps
+        self.set_n_taps(11*self.sps)
+        self.set_rrc_taps_rx(firdes.root_raised_cosine(1.0, self.sps, 1.0, self.excess_bw, self.n_taps))
+        self.set_rrc_taps_tx(firdes.root_raised_cosine(self.sps, self.sps, 1.0, self.excess_bw, self.n_taps))
+
+    def get_n_taps(self):
+        return self.n_taps
+
+    def set_n_taps(self, n_taps):
+        self.n_taps = n_taps
+        self.set_rrc_taps_rx(firdes.root_raised_cosine(1.0, self.sps, 1.0, self.excess_bw, self.n_taps))
+        self.set_rrc_taps_tx(firdes.root_raised_cosine(self.sps, self.sps, 1.0, self.excess_bw, self.n_taps))
+        self.fir_filter_xxx_0.set_taps(firdes.root_raised_cosine(1.0, 4, 1.0, self.excess_bw, self.n_taps))
+        self.interp_fir_filter_xxx_0.set_taps(firdes.root_raised_cosine(1.0, 4, 1.0, self.excess_bw, self.n_taps))
+
+    def get_excess_bw(self):
+        return self.excess_bw
+
+    def set_excess_bw(self, excess_bw):
+        self.excess_bw = excess_bw
+        self.set_rrc_taps_rx(firdes.root_raised_cosine(1.0, self.sps, 1.0, self.excess_bw, self.n_taps))
+        self.set_rrc_taps_tx(firdes.root_raised_cosine(self.sps, self.sps, 1.0, self.excess_bw, self.n_taps))
+        self.fir_filter_xxx_0.set_taps(firdes.root_raised_cosine(1.0, 4, 1.0, self.excess_bw, self.n_taps))
+        self.interp_fir_filter_xxx_0.set_taps(firdes.root_raised_cosine(1.0, 4, 1.0, self.excess_bw, self.n_taps))
+
+    def get_timing_delay(self):
+        return self.timing_delay
+
+    def set_timing_delay(self, timing_delay):
+        self.timing_delay = timing_delay
+
+    def get_samp_rate(self):
+        return self.samp_rate
+
+    def set_samp_rate(self, samp_rate):
+        self.samp_rate = samp_rate
+        self.blocks_throttle2_0.set_sample_rate(self.samp_rate)
+        self.qtgui_eye_sink_x_0.set_samp_rate(self.samp_rate)
+
+    def get_rrc_taps_tx(self):
+        return self.rrc_taps_tx
+
+    def set_rrc_taps_tx(self, rrc_taps_tx):
+        self.rrc_taps_tx = rrc_taps_tx
+
+    def get_rrc_taps_rx(self):
+        return self.rrc_taps_rx
+
+    def set_rrc_taps_rx(self, rrc_taps_rx):
+        self.rrc_taps_rx = rrc_taps_rx
+
+    def get_noise_amp(self):
+        return self.noise_amp
+
+    def set_noise_amp(self, noise_amp):
+        self.noise_amp = noise_amp
+
+
+
+
+def main(top_block_cls=phasekeyshifting, options=None):
+
+    qapp = Qt.QApplication(sys.argv)
+
+    tb = top_block_cls()
+
+    tb.start()
+    tb.flowgraph_started.set()
+
+    tb.show()
+
+    def sig_handler(sig=None, frame=None):
+        tb.stop()
+        tb.wait()
+
+        Qt.QApplication.quit()
+
+    signal.signal(signal.SIGINT, sig_handler)
+    signal.signal(signal.SIGTERM, sig_handler)
+
+    timer = Qt.QTimer()
+    timer.start(500)
+    timer.timeout.connect(lambda: None)
+
+    qapp.exec_()
+
+if __name__ == '__main__':
+    main()
